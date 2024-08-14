2e0668eed5d26495c97f87341b93d2550cc0af96
Neato <49599979+neatodev@users.noreply.github.com>
2024-08-15 00:07:55 +0200

    Add custom feature

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace ARCToolMultiPacker
 {
@@ -18,67 +20,117 @@ namespace ARCToolMultiPacker
         private static readonly string Re6Args = "-re6 -alwayscomp -pc -txt -v 7"; //tex
         private static readonly string RevArgs = "-rev -alwayscomp -pc -txt -v 7"; //tex
         private static readonly string Rev2Args = "-rev2 -texRE6 -alwayscomp -pc -txt -v 7"; //tex
+        private static string CustomArgs = "";
 
         private static string[] folderList;
+        private static List<string> CustomFolderList = new List<string>();
+        private static string[] AllArgs = { "-dd", "-texRE6", "-alwayscomp", "-pc", "-texRE5", "-dmc4se", "-txt", "-v", "7", "-rev2", "-rev", "-re6", "-re5", "-rehd", "-re0", "-ddo", "-dd", "-noxmemmagic", "-texDMC4SE", "-xmem", "-tex", "-dmc4" };
 
         private static string GameArgs;
         private static bool unpack;
         private static bool tex;
+        private static bool custom = false;
 
         /// <summary>
         /// Format: .\ARCToolMultiPacker.exe [game] [tex(optional)] [unpack/repack] [folders] 
-        /// Written by Mario Schweidler (frofoo/neatodev)
+        /// Alternative: .\ARCToolMultiPacker.exe [custom] [write arguments as you would in ARCTool] [folders]
+        /// Written by github.com/neatodev
         /// </summary>
-        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             DefineParams(args);
-            foreach (var arg in folderList)
-                if (unpack && tex)
+            if (custom)
+            {
+                foreach (string Folder in CustomFolderList)
                 {
-                    var files = Directory.GetFiles(Dir + "\\" + arg);
+                    var files = Directory.GetFiles(Dir + "\\" + Folder);
                     foreach (var file in files)
                         try
                         {
-                            Process.Start(".\\ARCtool.exe", GameArgs + " -tex " + file);
+                            Process.Start(".\\ARCtool.exe", CustomArgs + " " + file);
                         }
                         catch (FileNotFoundException e)
                         {
                             Console.WriteLine("Could not extract file. Is the path valid? " + e);
                         }
                 }
-                else if (unpack)
-                {
-                    var files = Directory.GetFiles(Dir + "\\" + arg);
-                    foreach (var file in files)
-                        try
-                        {
-                            Process.Start(".\\ARCtool.exe", GameArgs + " " + file);
-                        }
-                        catch (FileNotFoundException e)
-                        {
-                            Console.WriteLine("Could not extract file. Is the path valid? " + e);
-                        }
-                }
-                else if (!unpack || (!unpack && tex))
-                {
-                    var folders = Directory.GetDirectories(Dir + "\\" + arg);
-                    foreach (var folder in folders)
-                        try
-                        {
-                            Process.Start(".\\ARCtool.exe", GameArgs + " " + folder);
-                        }
-                        catch (DirectoryNotFoundException e)
-                        {
-                            Console.WriteLine("Could not pack file. Is the path valid? " + e);
-                        }
-                }
+            }
+            else
+            {
+                foreach (var arg in folderList)
+                    if (unpack && tex)
+                    {
+                        var files = Directory.GetFiles(Dir + "\\" + arg);
+                        foreach (var file in files)
+                            try
+                            {
+                                Process.Start(".\\ARCtool.exe", GameArgs + " -tex " + file);
+                            }
+                            catch (FileNotFoundException e)
+                            {
+                                Console.WriteLine("Could not extract file. Is the path valid? " + e);
+                            }
+                    }
+                    else if (unpack)
+                    {
+                        var files = Directory.GetFiles(Dir + "\\" + arg);
+                        foreach (var file in files)
+                            try
+                            {
+                                Process.Start(".\\ARCtool.exe", GameArgs + " " + file);
+                            }
+                            catch (FileNotFoundException e)
+                            {
+                                Console.WriteLine("Could not extract file. Is the path valid? " + e);
+                            }
+                    }
+                    else if (!unpack || (!unpack && tex))
+                    {
+                        var folders = Directory.GetDirectories(Dir + "\\" + arg);
+                        foreach (var folder in folders)
+                            try
+                            {
+                                Process.Start(".\\ARCtool.exe", GameArgs + " " + folder);
+                            }
+                            catch (DirectoryNotFoundException e)
+                            {
+                                Console.WriteLine("Could not pack file. Is the path valid? " + e);
+                            }
+                    }
+            }
         }
 
         private static void DefineParams(string[] args)
         {
             string[] argList = args;
+            foreach (var arg in argList)
+            {
+                Console.WriteLine(arg);
+            }
+
             DefineGame(argList[0]);
+
+            if (custom)
+            {
+                for (int i = 1; i < argList.Length; i++)
+                {
+                    bool IsFolder = true;
+                    foreach (string GArg in AllArgs)
+                    {
+                        if(GArg.ToLower() == argList[i].ToLower())
+                        {
+                            CustomArgs = CustomArgs + " " + argList[i];
+                            IsFolder = false;
+                        }
+                    }
+                    if (IsFolder)
+                    {
+                        CustomFolderList.Add(argList[i]);
+                    }
+                    IsFolder = true;
+                }
+                return;
+            }
 
             switch (argList[1])
             {
@@ -130,6 +182,9 @@ namespace ARCToolMultiPacker
         {
             switch (arg)
             {
+                case "custom":
+                    custom = true;
+                    break;
                 case "dd":
                     GameArgs = DdArgs;
                     break;
