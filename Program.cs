de3b9e9d4cd594d77672ef59cbb726d5a0a1059e
Neato <49599979+neatodev@users.noreply.github.com>
2024-09-07 19:31:52 +0200

    Add more checks

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Xml.Linq;
 
 namespace ARCToolMultiPacker
 {
@@ -41,18 +42,71 @@ namespace ARCToolMultiPacker
             DefineParams(args);
             if (custom)
             {
+                int SDirs = 0;
+                int Delay = 0;
+                string Key = "";
                 foreach (string Folder in CustomFolderList)
                 {
-                    var files = Directory.GetFiles(Dir + "\\" + Folder);
-                    foreach (var file in files)
-                        try
+                    var SubDirs = Directory.GetDirectories(Folder,"*", SearchOption.AllDirectories);
+                    SDirs = SDirs + SubDirs.Length;
+                }
+
+                if (SDirs > 0)
+                {
+                    Console.WriteLine("Detected a total of " + SDirs + " subfolder(s). Would you like to use ARCTool recursively? (May heavily increase runtime/running processes.) (Y/N)");
+                    Key = Console.ReadLine();
+                    while (Key != "Y".ToLower() && Key != "N".ToLower())
+                    {
+                        Key = Console.ReadLine();
+                    }
+                }
+
+                Console.WriteLine("Would you like to set a delay between the launch of ARCTool instances? (Y/N)");
+                string DelayKey = Console.ReadLine();
+                while (DelayKey != "Y".ToLower() && DelayKey != "N".ToLower())
+                {
+                    DelayKey = Console.ReadLine();
+                }
+                if (DelayKey == "Y".ToLower())
+                {
+                    Console.WriteLine("Please specify the delay from a range of 0 to 25 (seconds).");
+                    do
+                    {
+                        Int32.TryParse(Console.ReadLine(), out Delay);
+                        if (Delay > 25 || Delay < 0)
+                            Console.WriteLine("Please enter a number between 0 and 25.");
+
+                    } while (Delay > 25 || Delay < 0);
+                    Delay = Delay * 1000;
+                }
+
+                if (Key != "Y".ToLower())
+                {
+                    foreach (string Folder in CustomFolderList)
+                    {
+                       foreach (string File in Directory.EnumerateFiles(Folder, "*",SearchOption.AllDirectories))
                         {
-                            Process.Start(".\\ARCtool.exe", CustomArgs + " " + file);
+                            Thread.Sleep(Delay);
+                            Process.Start(".\\ARCtool.exe", CustomArgs + " " + File);
                         }
-                        catch (FileNotFoundException e)
-                        {
-                            Console.WriteLine("Could not extract file. Is the path valid? " + e);
-                        }
+                    }
+                } 
+                else
+                {
+                    foreach (string Folder in CustomFolderList)
+                    {
+                        var files = Directory.GetFiles(Dir + "\\" + Folder);
+                        foreach (var file in files)
+                            try
+                            {
+                                Thread.Sleep(Delay);
+                                Process.Start(".\\ARCtool.exe", CustomArgs + " " + file);
+                            }
+                            catch (FileNotFoundException e)
+                            {
+                                Console.WriteLine("Could not extract file. Is the path valid? " + e);
+                            }
+                    }
                 }
             }
             else
